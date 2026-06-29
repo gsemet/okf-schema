1bb4cec42850b656e68f0c8a6051113c7db1028d
Gaetan Semet <gaetan@xeberon.net>
2026-06-29 17:27:56 +0200

    chore: clean initial request
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- .agents/changes/request/init/07-git-squash-commits.sh
+++
@@ -1,2 +0,0 @@
-GIT_SEQUENCE_EDITOR="sed -i '1s/^pick /pick /; 2,$s/^pick /fixup /'" git rebase -i a16eb8ab7185606e5cb60730c46ad8e52f71d2f3
-git commit --amend -s -F /Users/az02065/Projects/DevTools/okf-schema/.agents/changes/request/init/07-commit-msg.md
